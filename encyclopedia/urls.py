521b617f66399b422e6806fe70d20d42fc6952c1
montresor34 <8246292+montresor34@users.noreply.github.com>
2021-01-08 16:30:22 +0000

    Add links to index page; add app name

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = "wiki"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:title>", views.entry_page, name="entry")
+    path("wiki/<str:title>", views.entry_page, name="entry_page")
 ]
